4838e73913430c20add44689bb9adbcd3f28cffe
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-07 11:57:12 -0300

    :sparkles: feat: add os projetos trybewallet e trybetunes

--- src/db/frontend.js
+++ src/db/frontend.js
@@ -1,4 +1,6 @@
 import solarSistem from './img/solar-system.gif'
+import trybetunes from './img/trybetunes.gif'
+import trybewallet from './img/trybewallet.gif'
 
 export default [
   {
@@ -6,5 +8,17 @@ export default [
     "url": "https://github.com/Jacqueline-Silva/solar-system",
     "src": solarSistem,
     "title": "solar-system"
+  },
+  {
+    "id": 2,
+    "url": "https://github.com/Jacqueline-Silva/trybetunes",
+    "src": trybetunes,
+    "title": "trybetunes"
+  },
+  {
+    "id": 3,
+    "url": "https://github.com/Jacqueline-Silva/trybe-wallet",
+    "src": trybewallet,
+    "title": "trybe-wallet"
   }
 ]
