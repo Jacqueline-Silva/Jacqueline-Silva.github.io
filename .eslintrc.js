b257838f4a694d0ba7c74237bb815d37acb71d11
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-11 13:23:01 -0300

    :wrench: chore: reconfiguração de ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'jsx-quotes': [2, 'prefer-single'],
+    quotes: [2, 'single'],
+  },
+};
